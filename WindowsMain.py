cbe94518a95780175545e6b05ba0a1445e635729
Nara295 <2953774108@qq.com>
2023-05-09 12:27:23 +0800

    Signed-off-by: Nara295 <2953774108@qq.com>

--- WindowsMain.py
+++ WindowsMain.py
@@ -1,26 +1,54 @@
 import yaml
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QProgressBar
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal
+from PyQt5.QtWidgets import QApplication, QWidget, QProgressBar, QVBoxLayout, QLabel, QDialog
+from PyQt5.QtGui import QIcon, QPainter, QBrush, QFont
+from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal, QRect
+
 
 class MyWidget(QWidget):
     def __init__(self):
         super().__init__()
+
         # 调用方法配置窗口标题和图标
         self.set_title_and_icon()
+
         # 定义窗口尺寸
         self.width = 1306
         self.height = 760
+
         # 调用方法设置窗口大小
         self.setFixedSize(self.width, self.height)
+
         # 初始化进度条并标记为关闭状态
         self.progress = QProgressBar(self)
         self.progress.setGeometry(0, 0, self.width, 50)
         self.progress.setValue(0)
         self.progress.hide()
-        # 注册关闭事件
-        self.closeEvent = self.on_close
+        
+        # 添加底部显示框
+        self.bottom_box = QWidget(self)
+        
+        # 添加一个垂直布局管理器
+        layout = QVBoxLayout()
+        
+        # 添加一个标签
+        label = QLabel("底部显示框的内容")
+        layout.addWidget(label)
+        
+        # 将垂直布局管理器添加到底部显示框中
+        self.bottom_box.setLayout(layout)
+
+        # 显示区域比例更改为9：4
+        bottom_box_width = int(self.height * 9 / 4 - 24)
+        bottom_box_height = int(self.height - 24)
+
+        self.bottom_box.setGeometry(12, self.height - bottom_box_height - 12, bottom_box_width, bottom_box_height)
+
+        # 显示区域四角做圆角处理
+        self.bottom_box.setAttribute(Qt.WA_TranslucentBackground)
+        # 修改此处，使显示区域底色透明
+        self.bottom_box.setStyleSheet("background-color: rgba(0, 0, 0, 0); border-radius: 5px;")
+
     # 定义方法读取并设置标题和图标
     def set_title_and_icon(self):
         with open('config.yaml', 'r') as f:
@@ -36,29 +64,37 @@ class MyWidget(QWidget):
                     pass
             except yaml.YAMLError:
                 print("配置为空或者错误")
-    # 定义关闭事件的方法
+
+    # 修改此处，关闭进度条的方法改为弹出窗口
     def on_close(self, event):
-        # 显示进度条
-        self.progress.show()
-        # 定义关闭进度条的方法
-        def close_bar():
-            self.progress.setValue(self.progress.value() + 1)
-            if self.progress.value() >= 100:
-                timer.stop()
-                print("窗口关闭")
-                app.exit()
-        # 定义定时器
-        timer = QBasicTimer()
-        self.step = 0
-        # 启动定时器
-        timer.start(30, self)
-        # 启动进度条
-        self.progress.setMaximum(100)
-        self.progress.setValue(0)
-        while self.progress.value() < 100:
-            close_bar()
-        # 阻塞主线程直到进度条结束
-        app.exec_()
+        # 创建进度条窗口实例
+        dialog = QDialog(self)
+        # 将进度条窗口大小设置为380*120，置顶窗口
+        dialog.setFixedSize(380, 120)
+        dialog.setWindowFlags(Qt.WindowStaysOnTopHint)
+
+        # 在进度条窗口中添加进度条和文本
+        progress = QProgressBar(dialog)
+        # 设置进度条最大值为100，进度条颜色为绿色
+        progress.setMaximum(100)
+        progress.setStyleSheet("QProgressBar::chunk {background-color: #1faa00;}")
+        # 设置进度条的位置为垂直左右居中，已知进度条大小为360*25
+        progress.setGeometry((dialog.width() - 360) // 2, (dialog.height() - 25) // 2, 360, 25)
+        
+        # 设置进度条文本
+        text = QLabel(dialog)
+        text.setAlignment(Qt.AlignCenter)
+        font = QFont()
+        font.setPointSize(10)
+        text.setFont(font)
+        # 设置文本大小为360*25，位置紧贴着进度条显示文案“关闭中...”
+        # 修改此处，将文本位置设置在进度条之上，位置居中
+        text.setGeometry((dialog.width() - 360) // 2, progress.y() - 35, 360, 25)
+        text.setText("关闭中...")
+        
+        # 显示进度条窗口
+        dialog.exec_()
+
     # 定义定时器事件
     def timerEvent(self, event):
         self.step += 1
@@ -66,11 +102,15 @@ class MyWidget(QWidget):
         if self.step >= 100:
             self.timer.stop()
 
+
 # 创建应用程序实例
 app = QApplication(sys.argv)
+
 # 创建窗口实例
 window = MyWidget()
+
 # 显示窗口
 window.show()
+
 # 进入主循环
 sys.exit(app.exec_())
