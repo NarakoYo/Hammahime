52509f1f1320ddcc778a233eee2d245fd1345985
Nara295 <2953774108@qq.com>
2023-05-22 12:21:54 +0800

    采用conde环境

--- start.bat
+++ start.bat
@@ -1,7 +1,7 @@
  
 @echo off
 
-python main.py
+call .\running\Scripts\activate.bat&&.\running\python.exe .\main.py&&pause
 
 REM This code will prevent the command prompt window from closing after the program has finished running
 pause
