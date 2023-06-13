cd810fd3b6dbbb16b418e23e2f76f5bdd3bf8005
Nara295 <2953774108@qq.com>
2023-06-13 20:11:04 +0800

    wallpapers
    想做个原神开门的视频壁纸，笑死

---
+++ wallpapers-Genshin/Carousel.js
@@ -0,0 +1,43 @@
+
+var screenWidth = window.screen.width;
+var screenHeight = window.screen.height;
+
+document.write("Screen resolution: " + screenWidth + "x" + screenHeight + "<br>");
+
+document.documentElement.style.width = screenWidth + "px";
+document.documentElement.style.height = screenHeight + "px";
+
+class MediaFiles {
+  constructor() {
+    this.files = [];
+  }
+
+  readFiles() {
+    const allowedExtensions = ['.png', '.jpg', '.webp', '.mp4', '.wav'];
+    const folder = 'Video';
+
+    fetch(folder)
+      .then(response => response.text())
+      .then(data => {
+        const parser = new DOMParser();
+        const htmlDocument = parser.parseFromString(data, 'text/html');
+        const links = htmlDocument.querySelectorAll('a');
+
+        links.forEach(link => {
+          const fileName = link.textContent;
+          const fileExtension = fileName.slice(fileName.lastIndexOf('.'));
+
+          if (allowedExtensions.includes(fileExtension)) {
+            this.files.push(fileName);
+          }
+        });
+
+        return this.files;
+      });
+  }
+}
+
+const mediaFiles = new MediaFiles();
+mediaFiles.readFiles();
+
+document.write(mediaFiles.readFiles + "<br>")
